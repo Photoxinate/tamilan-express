e8d13d9224a4653319684062330606dd5d701233
Shiroshan <47041898+siroshan@users.noreply.github.com>
2021-02-22 17:04:07 +0530

    container edited

--- components/ProductContainer/ProductContainer.js
+++ components/ProductContainer/ProductContainer.js
@@ -3,17 +3,17 @@ import ProuctCard from '../ProductCard/ProductCard';
 import classes from './ProductContainer.module.scss';
 
 export const ProductContainer = (props) => {
-    const width = 100/(props.itemsPerRow + 1)
-    const myStyle = {
-        boxSizing:'border-box',
-        width: width+'%',
-        margin: '5px',
-        padding: '5px'
-    }
+    // const width = 100/(props.itemsPerRow + 1)
+    // const myStyle = {
+    //     boxSizing:'border-box',
+    //     width: width+'%',
+    //     margin: '5px',
+    //     padding: '5px'
+    // }
   return (
     <div className={classes.container}>
       {props.products.map((product, i) => (
-        <div key={product.id} className={classes.wrap} style={myStyle}>
+        <div key={product.id} className={classes.wrap} >
           <ProuctCard product={product} />
         </div>
       ))}
