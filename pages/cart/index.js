18b11095e6c942dcc9934fa71c3b2a4a228ab188
Shiroshan <47041898+siroshan@users.noreply.github.com>
2021-02-28 17:36:32 +0530

    sub total added

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import CartTable from '../../components/CartTable/CartTable';
-import Cheque from '../../components/Cheque/Cheque';
-import classes from './Cart.module.scss';
+import SubTotal from '../../components/SubTotal/SubTotal';
+import classes from './index.module.scss';
 
 const index = (props) => {
-  if ((props.products === undefined || props.products.length === 0)) {
+  if (props.products === undefined || props.products.length === 0) {
     return <div>No item Added, Start Shopping</div>;
   } else {
     return (
       <div className={classes.wrap}>
-        <CartTable />
+        <div className={classes.colCart}>
+          <CartTable />
+        </div>
+        <div className={classes.colSub}>
+          <SubTotal />
+        </div>
       </div>
     );
   }
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ContactForm from '../../components/Forms/ContactForm/ContactForm'
+import classes from './index.module.scss'
 export const index = () => {
     return (
-        <div>
+        <div className={classes.wrap}>
             <ContactForm/>
         </div>
     )
